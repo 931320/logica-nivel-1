9fff8996a59fbf185c1093f85a51488de8038c4a
931320 <co8930568@gmail.com>
2025-04-15 21:22:24 -0300

    ext.05.py

---
+++ ext.05.py
@@ -0,0 +1,8 @@
+5. Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+ALUNO(A)          NOTA
+=========         =====
+ALINE              9.0
+MÁRIO              DEZ
+SHIRLEY            7.0
+
+=== Code Execution Successful ===
