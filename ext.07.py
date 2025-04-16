d494bb190d4d2b19eed3d6ec8f15bb64156b76a6
931320 <co8930568@gmail.com>
2025-04-15 21:40:34 -0300

    ext.07.py

---
+++ ext.07.py
@@ -0,0 +1,14 @@
+Enriqueça o desenho com outros caracteres, simulando enfeites.
+       X
+      XXX
+     XXXXX
+    XXXXXXX
+   XXXXXXXXX
+  XXXXXXXXXXX
+ XXXXXXXXXXXXX
+XXXXXXXXXXXXXXX
+       XX
+       XX
+      XXXX
+
+=== Code Execution Successful ===
