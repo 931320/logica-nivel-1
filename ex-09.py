3ecbde117d9e0176bc71b0c614c9babd87033a06
931320 <co8930568@gmail.com>
2025-05-20 21:09:45 -0300

    Update ex-09.py

--- ex-09.py
+++ ex-09.py
@@ -1,1 +1,10 @@
+#9 - escreva um software que some dois números.
+# Entrada dos números
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
 
+# Soma dos números
+resultado = numero1 + numero2
+
+# Exibe o resultado
+print(f"A soma de {numero1} e {numero2} é {resultado}.")
